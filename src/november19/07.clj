187dc2f8b4ebfab3f909ca89eb0929ea9d9df2b9
JohnnyJayJay <johnnyjayjay02@gmail.com>
2019-11-08 21:05:33 +0100

    Fix erronous end condition in 07, document 07

--- src/november19/07.clj
+++ src/november19/07.clj
@@ -16,23 +16,27 @@
 ; The top left corner and bottom right corner will always be 0.
 (ns november19.07)
 
+; Returns the element of the matrix that is at the specified x-y position, or 1 if the position is out of bounds
 (defn position [matrix x y]
   (-> matrix (get y []) (get x 1)))
 
+; Whether there is a 1 at the specified position in the matrix
 (defn blocked? [matrix x y]
   (= 1 (position matrix x y)))
 
+; Returns whether x-y represents the very last position of the given matrix (bottom right corner)
 (defn end? [matrix x y]
-    (and (> y (count matrix)) (> x (count (last matrix)))))
+    (and (= y (dec (count matrix))) (= x (dec (count (last matrix))))))
 
-; FIXME not working yet
+; Recursively calculates the amount of paths in a matrix that lead from a starting position x-y
+; to the very last position (bottom right corner). 1's represent borders, 0's ways to pass through.
 (defn paths
   ([matrix x y n]
    (cond
-     (end? matrix x y) 1
-     (blocked? matrix x y) 0
-     :else (+ n (paths matrix (inc x) y n) (paths matrix x (inc y) n))))
-  ([matrix] (paths matrix 0 0 0)))
+     (blocked? matrix x y) 0 ; If there is a border at this position, return 0
+     (end? matrix x y) 1 ; If this position is the last position, return 1
+     :else (+ n (paths matrix (inc x) y n) (paths matrix x (inc y) n)))) ; Else: the amount of paths is increased by the amount of paths starting from one position below the current position and one position right of the current position
+  ([matrix] (paths matrix 0 0 0))) ; Overload that starts at the top left corner with 0 paths found so far
 
 (println (paths [[0 0 1]
                  [0 0 1]
