c1d1683da8225503184b0aae9dc0e668b9f74889
JohnnyJayJay <johnnyjayjay02@gmail.com>
2019-11-13 22:17:23 +0100

    Add solution for 13

---
+++ src/november19/13.clj
@@ -0,0 +1,8 @@
+; You are given an array of length n + 1 whose elements belong to the set {1, 2, ..., n}.
+; By the pigeonhole principle, there must be a duplicate. Find it in linear time and space.
+(ns november19.13)
+
+(defn find-duplicate [coll]
+  (- (apply + coll) (apply + (range 1 (count coll))))) ; sum of all numbers in coll - sum of all integers from 1 to n = duplicate
+
+(println (find-duplicate [1 2 3 4 7 5 6 7 8 9]))
