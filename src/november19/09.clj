c364ddf91df696005f514affcd674a71d00fdcf4
JohnnyJayJay <johnnyjayjay02@gmail.com>
2019-11-16 19:24:40 +0100

    Fix 09

--- src/november19/09.clj
+++ src/november19/09.clj
@@ -16,31 +16,47 @@
 ; The path does not have to pass through the root, and each node can have any amount of children.
 (ns november19.09)
 
-(defrecord Node [parent? weight children])
+; TODO improve this
 
 ; returns the given node without the given child (useful to "block" certain ways down a tree)
 (defn without-child [node child]
-  (assoc node :children (remove #(identical? child %) (:children node))))
+  (assoc node :children (remove #(identical? child %) (node :children))))
 
 ; Starting from any node, work all possible ways down, not limited to leaf nodes as the last nodes
 (defn longest-path-down [root total-weight]
-  (let [children (:children root)]
+  (let [children (root :children)]
     (if (seq children)
       (apply
-        max ; if children available, return the maximum value of the longest-path-down from all children
+        max                                                 ; if children available, return the maximum value of the longest-path-down from all children
         (for [child children]
-          (let [weight (+ total-weight (:weight child))]
-            (max weight (longest-path-down child weight))))) ; stop at this node if the total weight is larger than any further path
-      total-weight))) ; if at leaf, return the total weight acquired along the way
+          (max total-weight
+               (longest-path-down child (+ total-weight (:weight child)))))) ; stop at this node if the total weight is larger than any further path
+      total-weight)))                                       ; if at leaf, return the total weight acquired along the way
 
-; The time complexity for this is stupidly large, but it handles all possible cases (including negative weight)
-(defn longest-path
-  ([node previous total-weight] ; node: node to work up from; previous: node in last recursion; total-weight: weight acquired so far
-   (let [parent (:parent node)]
-     (if parent ; if not at tree root
-       (let [weight (:weight node)]
-         (max ; find the bigger value of going back down now or going further up (recursively)
-           (longest-path parent node (+ weight total-weight))
-           (longest-path-down (without-child node previous) total-weight)))
-       (longest-path-down (without-child node previous) total-weight)))) ; if at root start going back down
-  ([tree] (apply max (longest-path tree nil 0) (for [child (:children tree)] (longest-path child))))) ; for all nodes, find the longest path that starts with them and from that extract the longest one
+; The time complexity for this should theoretically be O(n²) but is currently rather something
+; like O(n!) because of this bad implementation.
+; At least it handles all possible cases (including negative weights).
+(defn longest-path [tree]
+  (apply max
+         (apply + (take 2 (reverse (sort (filter (partial < 0) (for [child (tree :children)]
+                                                                 (longest-path-down child (child :weight))))))))
+         (for [child (tree :children)] (longest-path child)))) ; for all nodes, find the longest path that starts with them and from that extract the longest one
+
+
+(def tree
+  {:weight   0
+   :children [{:weight   3
+               :children []}
+              {:weight   5
+               :children []}
+              {:weight   8
+               :children [{:weight   2
+                           :children [{:weight   1
+                                       :children []}
+                                      {:weight   1
+                                       :children []}]}
+                          {:weight   4
+                           :children []}]}]
+   })
+
+(println (longest-path tree))
