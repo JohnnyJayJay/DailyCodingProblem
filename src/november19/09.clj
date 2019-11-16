ef7b4026a45fab98de7218348b8f4dded8522761
JohnnyJayJay <johnnyjayjay02@gmail.com>
2019-11-16 17:33:16 +0100

    Comment 09, remove unused function

--- src/november19/09.clj
+++ src/november19/09.clj
@@ -18,36 +18,29 @@
 
 (defrecord Node [parent? weight children])
 
+; returns the given node without the given child (useful to "block" certain ways down a tree)
 (defn without-child [node child]
   (assoc node :children (remove #(identical? child %) (:children node))))
 
-(defn leaves [tree]
-  (loop [updated tree found []]
-    (let [children (:children updated)]
-      (if (seq children)
-        (let [child (first children)]
-          (recur (without-child updated child)
-                 (into found (leaves child))))
-        found))))
-
+; Starting from any node, work all possible ways down, not limited to leaf nodes as the last nodes
 (defn longest-path-down [root total-weight]
   (let [children (:children root)]
     (if (seq children)
       (apply
-        max
+        max ; if children available, return the maximum value of the longest-path-down from all children
         (for [child children]
           (let [weight (+ total-weight (:weight child))]
-            (max weight (longest-path-down child weight)))))
-      total-weight)))
+            (max weight (longest-path-down child weight))))) ; stop at this node if the total weight is larger than any further path
+      total-weight))) ; if at leaf, return the total weight acquired along the way
 
 ; The time complexity for this is stupidly large, but it handles all possible cases (including negative weight)
 (defn longest-path
-  ([node previous total-weight]
+  ([node previous total-weight] ; node: node to work up from; previous: node in last recursion; total-weight: weight acquired so far
    (let [parent (:parent node)]
-     (if parent
+     (if parent ; if not at tree root
        (let [weight (:weight node)]
-         (max
+         (max ; find the bigger value of going back down now or going further up (recursively)
            (longest-path parent node (+ weight total-weight))
            (longest-path-down (without-child node previous) total-weight)))
-       (longest-path-down (without-child node previous) total-weight))))
-  ([tree] (apply max (longest-path tree nil 0) (for [child (:children tree)] (longest-path child nil 0)))))
+       (longest-path-down (without-child node previous) total-weight)))) ; if at root start going back down
+  ([tree] (apply max (longest-path tree nil 0) (for [child (:children tree)] (longest-path child))))) ; for all nodes, find the longest path that starts with them and from that extract the longest one
