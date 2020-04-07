3ab042cd0e1c2caa1d6e518cce6f49c38b5a2a2f
JohnnyJayJay <johnnyjayjay02@gmail.com>
2020-04-07 17:24:06 +0200

    Add solution of custom task in day 23

--- src/november19/23.clj
+++ src/november19/23.clj
@@ -1,9 +1,26 @@
-; Describe and give an example of each of the following types of polymorphism:
+; This is not a DailyCodingProblem task, but I came across the question elsewhere and thought
+; my solution to it might fit nicely into this collection.
 ;
-; Ad-hoc polymorphism
-; Parametric polymorphism
-; Subtype polymorphism
+; Given a list of options, generate all possible ordered combinations of n consecutive options.
+; E.g. options = [1 2] and n = 3 should result in 8 combinations:
+; ((1 1 1) (1 1 2) (1 2 1) (1 2 2) (2 1 1) (2 1 2) (2 2 1) (2 2 2))
 (ns november19.23)
 
-; TODO
+(defn make-roots [n options]
+  (if (pos? n)
+    (for [option options]
+      {:option option :children (make-roots (dec n) options)})
+    ()))
 
+(defn combinations
+  ([root]
+   (let [children (:children root)
+         option (:option root)]
+     (if (seq children)
+       (apply
+         concat
+         (map (fn [child] (map (fn [path] (conj path option)) (combinations child))) children))
+       (list (list option)))))
+  ([n & options] (apply concat (map combinations (make-roots n options)))))
+
+(println (combinations 3 1 2))
